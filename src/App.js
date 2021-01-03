38100a55c5ccbb28e463c4139e49f108c5f5ee02
Radovan Šurlák <radovan.surlak@gmail.com>
2021-01-03 19:51:14 +0100

    added a progress bar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import getYouTubeID from "get-youtube-id";
 import fileDownload from "js-file-download";
 import axios from "axios";
 
 function App() {
+  const [downloadPercentage, setDownloadPercentage] = useState(0);
+  const [isDownloading, setIsDownloading] = useState(false);
+
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const videoURL = urlParams.get("videoURL");
     let videoID;
 
     if (videoURL) {
+      setIsDownloading(true);
       videoID = getYouTubeID(videoURL);
 
       axios({
         url: `https://yeetube.herokuapp.com/download/${videoID}`,
+        // url: `http://localhost:4000/download/${videoID}`,
         method: "POST",
         responseType: "blob",
+        onDownloadProgress(progress) {
+          const percentageProgress = Math.round(
+            (progress.loaded / progress.total) * 100
+          );
+          setDownloadPercentage(percentageProgress);
+        },
       }).then((res) => {
+        setIsDownloading(false);
         fileDownload(res.data, res.headers.filename);
       });
     }
   }, []);
 
-  return <h1>This is an AHSOME YeeTube downloader baby</h1>;
+  return <>{isDownloading && <h1>{downloadPercentage}%</h1>}</>;
 }
 
 export default App;
